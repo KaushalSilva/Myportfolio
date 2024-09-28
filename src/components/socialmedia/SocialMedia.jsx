2f94f6360ba7e9ceaf6102c0186442dabe51b156
Kaushal <Kaushalsilva15@gmail.com>
2024-09-28 12:52:32 +0530

    Deploying to GitHub Pages

--- src/components/socialmedia/SocialMedia.jsx
+++ src/components/socialmedia/SocialMedia.jsx
@@ -11,10 +11,10 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 function SocialMedia() {
     return (
         <div className='socialmedia'>
-            <div className='socialmedia-icon' ><a href='https://www.linkedin.com/in/shashininn?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' target="_blank" ><LinkedInIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
-            <div className='socialmedia-icon' ><a href='https://github.com/Nathasha2001' target="_blank" ><GitHubIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
-            <div className='socialmedia-icon' ><a href='https://www.facebook.com/share/4mAXDfKJWx8F9wXk/?mibextid=LQQJ4d' target="_blank" ><FacebookIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
-            <div className='socialmedia-icon' ><a href='https://www.instagram.com/shashini_nathasha?igsh=ZG84YzhoaHBjdHdr&utm_source=qr' target="_blank" ><InstagramIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
+            <div className='socialmedia-icon' ><a href='www.linkedin.com/in/kaushal-silva-821a1831a' target="_blank" ><LinkedInIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
+            <div className='socialmedia-icon' ><a href='https://github.com/KaushalSilva' target="_blank" ><GitHubIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
+            <div className='socialmedia-icon' ><a href='https://www.facebook.com/share/ZzaNUUGjD6FtjZRy/?mibextid=qi2Omg' target="_blank" ><FacebookIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
+            <div className='socialmedia-icon' ><a href='https://www.instagram.com/_kaushalsilva_15?igsh=dTQza3k2NnM5Nzd4' target="_blank" ><InstagramIcon sx={{ color: '#fff' }} fontSize='large' /></a></div>
         </div>
     )
 }
