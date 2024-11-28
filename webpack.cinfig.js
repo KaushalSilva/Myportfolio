a23e2cbdf652e2a0289781543b0b886deff4ac08
Dushan Chanuka <Kaushalsilva15@gmail.com>
2024-11-29 02:32:21 +0530

    updatee

---
+++ webpack.cinfig.js
@@ -0,0 +1,40 @@
+import path from 'path';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html',
+        }),
+    ],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+    },
+};
